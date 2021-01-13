bf57df495198eeac74769065c2cfa2d1e94f419b
NesyElimelech <nesy15@gmail.com>
2021-01-13 23:33:48 +0200

    Add error handlers

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import { config } from 'dotenv';
 import connectDB from './config/db.js';
 import colors from 'colors';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -15,6 +16,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(
   port,
