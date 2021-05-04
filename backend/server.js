58a3aa601f97a4ab3e721f6b808bf3fe4d4ffe18
NesyElimelech <nesy15@gmail.com>
2021-05-04 17:50:26 +0300

    Remove BodyParser

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,6 @@ config();
 connectDB();
 app.use(express.static('.'));
 app.use(compression());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 if (process.env.NODE_ENV === 'development') {
