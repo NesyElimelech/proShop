309b0962069e6600287258bc01a0a5a6f11fb23a
NesyElimelech <nesy15@gmail.com>
2021-06-24 18:08:30 +0300

    Build the project

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import enforce from 'express-sslify';
 import path from 'path';
 import { config } from 'dotenv';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 import compression from 'compression';
 import connectDB from './config/db.js';
 import _colors from 'colors';
---
+++ frontend/build/precache-manifest.aa5802026e085aa4d64281e83f44bd33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c250a67dd78d1d2e383",
+    "url": "/static/css/main.1025104c.chunk.css"
+  },
+  {
+    "revision": "1c250a67dd78d1d2e383",
+    "url": "/static/js/main.f6e58dc3.chunk.js"
+  },
+  {
+    "revision": "54a09a62348e37e22e51",
+    "url": "/static/js/runtime~main.2bc4a4b7.js"
+  },
+  {
+    "revision": "318b47bc55d129af48cf",
+    "url": "/static/js/2.091e9d39.chunk.js"
+  },
+  {
+    "revision": "f1b248ec0a057248d417",
+    "url": "/static/js/3.1c9b81b3.chunk.js"
+  },
+  {
+    "revision": "9d6b2213122582385eca40ceefdb6a8b",
+    "url": "/index.html"
+  }
+];
